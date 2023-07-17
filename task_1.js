9f24bb319d755012ad20b645a4c90ad955a11731
K0SSSSS <139591247+K0SSSSS@users.noreply.github.com>
2023-07-17 17:24:54 +0300

    Add files via upload

---
+++ task_1.js
@@ -0,0 +1,9 @@
+const name = "Генератор"
+let price = 1000
+const result = `Обрано ${name}, ціна за одиницю ${price}
+кредитів`
+console.log(result);
+const price2 = price*2
+const result2 = `Обрано ${name}, ціна за одиницю ${price2}
+кредитів`
+console.log(result2);
---
+++ task_2.js
@@ -0,0 +1,7 @@
+const total = 100
+const ordered = 200
+if(ordered < total){
+    console.log("Замовлення оформлено");
+} else{
+    console.log("На складі недостатня кількість товару!");
+}
